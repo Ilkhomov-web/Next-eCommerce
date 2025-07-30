3c2eb5fe807b37f9b86c857c29dc45fa4259237b
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-07-30 20:13:46 +0500

    some change

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Navbar from "@/app/components/Navbar";
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return (
+    <Container maxWidth="xl">
+      <Navbar />
+    </Container>
+  );
+};
+
+export default page;
